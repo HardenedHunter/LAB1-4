0c9f2590264f8fc46b768182c237af5da1bc9794
Artyom Khudyakov <47335768+HardenedHunter@users.noreply.github.com>
2020-06-04 12:01:55 +0300

    Added brief description

--- C++/LAB2/LAB2/LAB2.cpp
+++ C++/LAB2/LAB2/LAB2.cpp
@@ -1,23 +1,20 @@
-﻿// ReSharper disable StringLiteralTypo
-#include <clocale>
-#include <cstdlib>
-#include <ctime>
-#include "console_utils.h"
+﻿// ReSharper disable CommentTypo
+// ReSharper disable StringLiteralTypo
 
+// 2. Список абонентов кабельной сети состоит из структур с полями : фамилия, район,
+// адрес, телефон, номер договора, дата заключения договора, оплата установки,
+// абонентская плата помесячно, дата последнего платежа.Поиск по фамилии,
+// району, дате заключения договора, дате последнего платежа.
+//
+// 3. Реализовать справочник абонентов кабельной сети.
+
+#include <clocale>
 #include "render_utils.h"
 
 using namespace render_utils;
-using namespace console_utils;
-
-void setup()
-{
-	setlocale(LC_ALL, "Russian");
-	srand(time(nullptr));
-	rand();
-}
 
 int main()
 {
-	setup();
+	setlocale(LC_ALL, "Russian");
 	render();
 }
